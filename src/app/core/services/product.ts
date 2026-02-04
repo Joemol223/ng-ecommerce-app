1be03a5667337f7ca6f6eef8fc0bc49b71ec8870
Joemol Joy <joemoljoy.a@gmail.com>
2026-02-04 14:53:33 -0500

    Fixed product service test page and images

--- src/app/core/services/product.ts
+++ src/app/core/services/product.ts
@@ -7,18 +7,18 @@ import { Observable, of } from 'rxjs';
 export class ProductService {
 
   private products = [
-    { id: 1, name: 'iPhone 15', price: 999, image: 'https://picsum.photos/300?1', category: 'Mobile' },
-    { id: 2, name: 'MacBook Pro', price: 1999, image: 'https://picsum.photos/300?2', category: 'Laptop' },
-    { id: 3, name: 'Headphones', price: 199, image: 'https://picsum.photos/300?3', category: 'Audio' },
-    { id: 4, name: 'Smart Watch', price: 299, image: 'https://picsum.photos/300?4', category: 'Wearable' },
-     { id: 5, name: 'iPhone 15', price: 999, image: 'https://picsum.photos/300?1', category: 'Mobile' },
-    { id: 6, name: 'MacBook Pro', price: 1999, image: 'https://picsum.photos/300?2', category: 'Laptop' },
-    { id: 7, name: 'Headphones', price: 199, image: 'https://picsum.photos/300?3', category: 'Audio' },
-    { id: 8, name: 'Smart Watch', price: 299, image: 'https://picsum.photos/300?4', category: 'Wearable' },
-     { id: 9, name: 'iPhone 15', price: 999, image: 'https://picsum.photos/300?1', category: 'Mobile' },
-    { id: 10, name: 'MacBook Pro', price: 1999, image: 'https://picsum.photos/300?2', category: 'Laptop' },
-    { id: 11, name: 'Headphones', price: 199, image: 'https://picsum.photos/300?3', category: 'Audio' },
-    { id: 12, name: 'Smart Watch', price: 299, image: 'https://picsum.photos/300?4', category: 'Wearable' }
+    { id: 1, name: 'iPhone 15', price: 999, image: 'https://images.pexels.com/photos/29020349/pexels-photo-29020349.jpeg', category: 'Mobile' },
+    { id: 2, name: 'MacBook Pro', price: 1999, image: 'https://images.pexels.com/photos/5912325/pexels-photo-5912325.jpeg', category: 'Laptop' },
+    { id: 3, name: 'Headphones', price: 199, image: 'https://images.pexels.com/photos/30428606/pexels-photo-30428606.jpeg', category: 'Audio' },
+    { id: 4, name: 'Smart Watch', price: 299, image: 'https://images.pexels.com/photos/5083218/pexels-photo-5083218.jpeg', category: 'Wearable' },
+     { id: 5, name: 'iPhone 15', price: 999, image: 'https://images.pexels.com/photos/29020349/pexels-photo-29020349.jpeg', category: 'Mobile' },
+    { id: 6, name: 'MacBook Pro', price: 1999, image: 'https://images.pexels.com/photos/5912325/pexels-photo-5912325.jpeg', category: 'Laptop' },
+    { id: 7, name: 'Headphones', price: 199, image: 'https://images.pexels.com/photos/30428606/pexels-photo-30428606.jpeg', category: 'Audio' },
+    { id: 8, name: 'Smart Watch', price: 299, image: 'https://images.pexels.com/photos/5083218/pexels-photo-5083218.jpeg', category: 'Wearable' },
+     { id: 9, name: 'iPhone 15', price: 999, image: 'https://images.pexels.com/photos/29020349/pexels-photo-29020349.jpeg', category: 'Mobile' },
+    { id: 10, name: 'MacBook Pro', price: 1999, image: 'https://images.pexels.com/photos/5912325/pexels-photo-5912325.jpeg', category: 'Laptop' },
+    { id: 11, name: 'Headphones', price: 199, image: 'https://images.pexels.com/photos/30428606/pexels-photo-30428606.jpeg', category: 'Audio' },
+    { id: 12, name: 'Smart Watch', price: 299, image: 'https://images.pexels.com/photos/5083218/pexels-photo-5083218.jpeg', category: 'Wearable' }
   ];
 
   getProducts(): Observable<any[]> {
