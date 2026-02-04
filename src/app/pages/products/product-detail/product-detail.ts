3b057b4d3e122fe3f0dd2ef3d06d08d785987fd5
Joemol Joy <joemoljoy.a@gmail.com>
2026-02-04 15:13:53 -0500

    craeted basic product detail page with absic features

--- src/app/pages/products/product-detail/product-detail.ts
+++ src/app/pages/products/product-detail/product-detail.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule, ActivatedRoute } from '@angular/router';
+import { ProductService } from '../../../core/services/product';
 
 @Component({
   selector: 'app-product-detail',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './product-detail.html',
   styleUrl: './product-detail.css',
 })
-export class ProductDetail {
+export class ProductDetail implements OnInit{
+ product: any;
+  loading = true;
+  quantity = 1; // default quantity
 
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {}
+
+  ngOnInit() {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.product = this.productService.getProductById(id);
+    this.loading = false;
+  }
+
+  increaseQuantity() {
+    if (this.quantity < 10) this.quantity++;
+  }
+
+  decreaseQuantity() {
+    if (this.quantity > 1) this.quantity--;
+  }
+
+  addToCart() {
+    alert(`${this.quantity} x ${this.product.name} added to cart!`);
+    // TODO: Connect to CartService
+  }
+
+  buyNow() {
+    alert(`Buying ${this.quantity} x ${this.product.name} now!`);
+    // TODO: Redirect to checkout page
+  }
 }
